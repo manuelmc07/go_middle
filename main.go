7353fb4a448a78f80daaa9b0b26bc6ff5eba919e
Manuel Antonio Mijangos Campos <>
2022-12-15 15:57:56 -0600

    add default struct with getters and setters

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import "fmt"
 
+// Employee public struct
+type Employee struct {
+	id   int
+	name string
+}
+
+// SetId public function
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
+// SetName public function
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+// GetId public function
+func (e *Employee) GetId() int {
+	return e.id
+}
+
+// GetName public function
+func (e *Employee) GetName() string {
+	return e.name
+}
+
+func main() {
+	// declare new instance of employee struct
+	e := Employee{}
+	// add setters
+	e.SetId(5)
+	e.SetName("Manuel")
+	// retrieve getters
+	fmt.Println(e.GetId())
+	fmt.Println(e.GetName())
 }
