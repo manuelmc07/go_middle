bd4c59437de871dc6407ca8accc24ca9f5ce624d
Manuel Antonio Mijangos Campos <>
2022-12-16 13:20:08 -0600

    add constructors to structs

--- main.go
+++ main.go
@@ -28,6 +28,13 @@ func (e *Employee) GetName() string {
 	return e.name
 }
 
+func NewEmployee(id int, name string) *Employee {
+	return &Employee{
+		id:   id,
+		name: name,
+	}
+}
+
 func main() {
 	// declare new instance of employee struct
 	e := Employee{}
@@ -37,4 +44,29 @@ func main() {
 	// retrieve getters
 	fmt.Println(e.GetId())
 	fmt.Println(e.GetName())
+
+	// 2
+	e2 := Employee{
+		id:   1,
+		name: "Luis",
+	}
+	fmt.Println(e2)
+
+	// 3
+	// whe used new keyword to instance will be work with pointer
+	e3 := new(Employee)
+	e3.id = 4
+	e3.name = "manuel"
+	fmt.Println(*e3)
+
+	// 4
+	e4 := Employee{}
+	e4.id = 2
+	e4.name = "Joss"
+	fmt.Println(e4)
+
+	// 5
+	e5 := NewEmployee(8, "Vivika")
+	fmt.Println(*e5)
+
 }
