79b126bdc8d8aa653fc479d972495f7f92f2939d
Manuel Antonio Mijangos Campos <>
2022-12-23 13:18:04 -0600

    add concurrenccy methods

---
+++ concurrency/sem.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	c := make(chan int, 1)
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		c <- 1
+		wg.Add(1)
+		go doSomething2(i, &wg, c)
+	}
+
+	wg.Wait()
+}
+
+func doSomething2(i int, wg *sync.WaitGroup, c chan int) {
+	defer wg.Done()
+	fmt.Printf("Id %d started\n", i)
+	time.Sleep(4 * time.Second)
+	fmt.Printf("Id %d end\n", i)
+
+	<-c
+}
