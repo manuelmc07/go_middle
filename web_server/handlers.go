64552aa3d823d0d2ad46a512ae38cedc0da54eca
Manuel Antonio Mijangos Campos <>
2022-12-24 17:58:45 -0600

    PROJECT SERVER WEB

---
+++ web_server/handlers.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func HandlerRoot(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	fmt.Fprintf(w, "Hello world")
+}
+
+func HandleHome(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	fmt.Fprintf(w, "This is the API endpoint")
+}
+
+func PostRequest(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	decoder := json.NewDecoder(r.Body)
+	var metadata MetaData
+	err := decoder.Decode(&metadata)
+
+	if err != nil {
+		fmt.Fprintf(w, "error %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "Payload %v\n", metadata)
+}
+
+func UserPostRequest(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	err := decoder.Decode(&user)
+
+	if err != nil {
+		fmt.Fprintf(w, "error %v", err)
+		return
+	}
+	response, err := user.ToJson()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
---
+++ web_server/types.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Middleware func(handlerFunc http.HandlerFunc) http.HandlerFunc
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
+
+func (u *User) ToJson() ([]byte, error) {
+	return json.Marshal(u)
+}
+
+type MetaData interface {
+}
